656ea9553c281e4acc4801cddd6af08a361d38cd
tofisvej <svejovsky@spaceknow.com>
2024-06-07 23:38:43 +0200

    menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [headerHeight, setHeaderHeight] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -26,34 +28,61 @@ export const Navbar = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div
       className={`navbar p-4 sm:p-6 fixed z-10 w-full transition-all duration-300 ${
         isScrolled ? 'bg-white shadow-lg' : 'bg-transparent header-shadow'
       }`}
     >
-      <div className="flex-1">
-        <a className={`text-2xl sm:text-3xl font-bold transition-colors duration-300 ${isScrolled ? 'text-black' : 'text-white'}`}>
-          ARABFEST
-        </a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1 space-x-4 sm:space-x-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className={`focus:outline-none transition-colors duration-300 ${isScrolled ? 'text-black' : 'text-white'}`}>
+              {isMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+            </button>
+          </div>
+          <a className={`ml-3 text-2xl sm:text-3xl font-bold transition-colors duration-300 ${isScrolled ? 'text-black' : 'text-white'}`}>
+            ARABFEST
+          </a>
+        </div>
+        <div className="hidden md:flex space-x-4 sm:space-x-6">
           {['hero', 'news', 'about', 'team', 'events', 'sponsors', 'contact', 'reservations'].map((section, index) => (
-            <li key={index}>
+            <Link
+              key={index}
+              to={section}
+              smooth={true}
+              duration={500}
+              offset={-headerHeight}
+              className={`transition-colors duration-300 ${isScrolled ? 'text-black' : 'text-white font-bold'}`}
+            >
+              {section.toUpperCase()}
+            </Link>
+          ))}
+        </div>
+      </div>
+      {isMenuOpen && (
+        <div className="md:hidden fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 z-20">
+          <div className="flex flex-col items-center justify-center h-full">
+            {['hero', 'news', 'about', 'team', 'events', 'sponsors', 'contact', 'reservations'].map((section, index) => (
               <Link
+                key={index}
                 to={section}
                 smooth={true}
                 duration={500}
                 offset={-headerHeight}
-                className={`transition-colors duration-300 ${isScrolled ? 'text-black' : 'text-white font-bold'}`}
+                className="text-white text-xl font-bold mb-6"
+                onClick={toggleMenu}
               >
                 {section.toUpperCase()}
               </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
