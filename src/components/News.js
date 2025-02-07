0dcc0ea7e9e39fe0fb4f928c6c7004cea1fb1b94
iDome89 <dominik.zecoli@gmail.com>
2025-02-07 09:36:26 +0100

    add conditional rendering for news section in News component

--- src/components/News.js
+++ src/components/News.js
@@ -49,6 +49,8 @@ export const News = () => {
   };
 
   return (
+  <>
+    {news.length > 0 ? (
     <div className="bg-gray-100 py-10">
       <h2 className="text-3xl text-center font-bold text-gray-800 mb-8">
         Novinky
@@ -99,6 +101,7 @@ export const News = () => {
           );
         })}
       </div>
-    </div>
+    </div>) : null}
+    </>
   );
 };
